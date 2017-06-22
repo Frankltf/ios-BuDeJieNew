89a4057c2cb2cd8e8f98d3ee551163da856b7db4
tengfei <langerfei@163.com>
2017-06-22 23:09:54 +0800

    网络请求

--- Budejienew/Budejienew/Classes/Ad/Controller/XMGAdViewController.m
+++ Budejienew/Budejienew/Classes/Ad/Controller/XMGAdViewController.m
@@ -8,7 +8,7 @@
 //
 
 #import "XMGAdViewController.h"
-
+#import <AFNetworking/AFNetworking.h>
 
 //#define XMGScreenW [UIScreen mainScreen].bounds.size.width
 //#define XMGScreenH [UIScreen mainScreen].bounds.size.height
@@ -24,7 +24,7 @@
 #define iphone6 (XMGScreenH == 667)
 #define iphone5 (XMGScreenH == 568)
 #define iphone4 (XMGScreenH == 480)
-
+#define code2 @"phcqnauGuHYkFMRquANhmgN_IauBThfqmgKsUARhIWdGULPxnz3vndtkQW08nau_I1Y1P1Rhmhwz5Hb8nBuL5HDknWRhTA_qmvqVQhGGUhI_py4MQhF1TvChmgKY5H6hmyPW5RFRHzuET1dGULnhuAN85HchUy7s5HDhIywGujY3P1n3mWb1PvDLnvF-Pyf4mHR4nyRvmWPBmhwBPjcLPyfsPHT3uWm4FMPLpHYkFh7sTA-b5yRzPj6sPvRdFhPdTWYsFMKzuykEmyfqnauGuAu95Rnsnbfknbm1QHnkwW6VPjujnBdKfWD1QHnsnbRsnHwKfYwAwiu9mLfqHbD_H70hTv6qnHn1PauVmynqnjclnj0lnj0lnj0lnj0lnj0hThYqniuVujYkFhkC5HRvnB3dFh7spyfqnW0srj64nBu9TjYsFMub5HDhTZFEujdzTLK_mgPCFMP85Rnsnbfknbm1QHnkwW6VPjujnBdKfWD1QHnsnbRsnHwKfYwAwiuBnHfdnjD4rjnvPWYkFh7sTZu-TWY1QW68nBuWUHYdnHchIAYqPHDzFhqsmyPGIZbqniuYThuYTjd1uAVxnz3vnzu9IjYzFh6qP1RsFMws5y-fpAq8uHT_nBuYmycqnau1IjYkPjRsnHb3n1mvnHDkQWD4niuVmybqniu1uy3qwD-HQDFKHakHHNn_HR7fQ7uDQ7PcHzkHiR3_RYqNQD7jfzkPiRn_wdKHQDP5HikPfRb_fNc_NbwPQDdRHzkDiNchTvwW5HnvPj0zQWndnHRvnBsdPWb4ri3kPW0kPHmhmLnqPH6LP1ndm1-WPyDvnHKBrAw9nju9PHIhmH9WmH6zrjRhTv7_5iu85HDhTvd15HDhTLTqP1RsFh4ETjYYPW0sPzuVuyYqn1mYnjc8nWbvrjTdQjRvrHb4QWDvnjDdPBuk5yRzPj6sPvRdgvPsTBu_my4bTvP9TARqnam"
 @interface XMGAdViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *launchImage;
 @property (weak, nonatomic) IBOutlet UIView *addContainerView;
@@ -37,7 +37,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setuplaunchImage];
+    [self loadAdData];
     // Do any additional setup after loading the view.
+}
+-(void)loadAdData{
+    AFHTTPSessionManager *mr=[AFHTTPSessionManager manager];
+    
+    NSMutableDictionary *parameters=[NSMutableDictionary dictionary];
+    parameters[@"code2"]=code2;
+    [mr GET:@"http://mobads.baidu.com/cpro/ui/mads.php" parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        NSLog(@"%@",responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"%@",error);
+    }];
 }
 -(void)setuplaunchImage{
     if(iphone6P){
