38650657f0ffbf7c1d15535bb33883265734a9c9
tengfei <langerfei@163.com>
2017-06-24 12:42:12 +0800

    success

--- Budejienew/Budejienew/Classes/New/Controller/XMGSubTagViewCell.m
+++ Budejienew/Budejienew/Classes/New/Controller/XMGSubTagViewCell.m
@@ -10,21 +10,20 @@
 #import "XMGSubTabItem.h"
 #import <UIImageView+WebCache.h>
 @interface XMGSubTagViewCell ()
-//@property (weak, nonatomic) IBOutlet UILabel *topTitle;
-//@property (weak, nonatomic) IBOutlet UILabel *bottomTitle;
-//@property (weak, nonatomic) IBOutlet UIImageView *imageface;
-
+@property (weak, nonatomic) IBOutlet UILabel *topTitle;
+@property (weak, nonatomic) IBOutlet UILabel *bottomTitle;
+@property (weak, nonatomic) IBOutlet UIImageView *imageface;
 
 @end
 
 
 @implementation XMGSubTagViewCell
-//-(void)setItem:(XMGSubTabItem *)item{
-//    _item=item;
-//    //_topTitle.text=item.theme_name;
-//    //_bottomTitle.text=item.sub_number;
-////    [_imageface sd_setImageWithURL:[NSURL URLWithString:item.image_list] placeholderImage:[UIImage imageNamed:@"defaultUserIcon"]];
-//}
+-(void)setItem:(XMGSubTabItem *)item{
+    _item=item;
+    _topTitle.text=item.theme_name;
+    _bottomTitle.text=item.sub_number;
+    [_imageface sd_setImageWithURL:[NSURL URLWithString:item.image_list] placeholderImage:[UIImage imageNamed:@"defaultUserIcon"]];
+}
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
