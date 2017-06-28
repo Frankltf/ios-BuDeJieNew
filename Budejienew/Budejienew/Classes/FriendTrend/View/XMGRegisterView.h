f6fb48b3d78ae4c9376e34c8e1923e207b245f69
tengfei <1536303470@qq.com>
2017-06-28 11:49:22 +0800

    输入框

---
+++ Budejienew/Budejienew/Classes/FriendTrend/View/XMGRegisterView.h
@@ -0,0 +1,13 @@
+//
+//  XMGRegisterView.h
+//  Budejienew
+//
+//  Created by admin on 2017/6/28.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XMGRegisterView : UIView
+
+@end
---
+++ Budejienew/Budejienew/Classes/FriendTrend/View/XMGRegisterView.m
@@ -0,0 +1,21 @@
+//
+//  XMGRegisterView.m
+//  Budejienew
+//
+//  Created by admin on 2017/6/28.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import "XMGRegisterView.h"
+
+@implementation XMGRegisterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
